317c1ed0eb168165f9e1c268a8367dc2914022e5
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-10-08 14:05:25 +0200

    Add project files.

---
+++ uppgift 2,2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hur långt kan Elin hoppa i längdhopp. Mätt i meter?");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt kan Alma hoppa i längdhopp. Mätt i meter?");
+            int tal2 = int.Parse(Console.ReadLine());
+            int summa = tal1 - tal2;
+            Console.WriteLine("Elin hoppade " + summa + " meter längre än Alma");
+        }
+    }
+}
